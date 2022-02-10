4f39e4fce4eed1b3477960e5325c3912a7752c9f
Ivan Otarola <iotarolal@gmail.com>
2022-02-10 03:10:29 -0300

    lista usuarios por consola y por server

--- server.js
+++ server.js
@@ -3,31 +3,38 @@ const moment = require('moment');
 const axios = require('axios');
 const chalk = require('chalk');
 const uuid = require('uuid');
+const { restart } = require('nodemon');
 
 const app = express();
 
 // acá realizamos los requerimientos del desafío
-
-app.get('/', (req, res) => {
-    res.end('lista de usuario')
-})
-
 let arrayuser = [];
 
+app.get('/', (req, res) => {
+    let listauser="";
+    for (usuario of arrayuser) {
+        listauser = listauser + `${usuario}`;
+        console.log(chalk.blue.bgWhite(`${usuario}`));
+    }
+    res.end(`${listauser}`);
+})
+
 app.listen(3000, async() => {
-    console.log(chalk.green.bgYellow('servidor funcionando en el puerto 3000'));
-    console.log('Ejemplo de identificado único', uuid.v4());
+    console.log('servidor funcionando en el puerto 3000');
+
+    // pedir los datos con axios para 10 usuarios
     const resultado = await axios.get('https://randomuser.me/api/?results=10');
     const usuarios = resultado.data.results; 
-//    console.log(resultado.data.results);
-//    const usuarios = resultado.data.data;
+
+// iterar por cada usuario , agrear a un array, aplicar moment y obtener uuid
     for (usuario of usuarios) {
         const ahora =  moment().format('MMMM Do YYYY, hh:MM:SSa');
         const id = uuid.v4();
-        arrayuser.push(`Nombre:${usuario.name.first}-Apellido:${usuario.name.last}-ID:${id.length-6}`);
-
-
-
+        arrayuser.push(`Nombre:${usuario.name.first}-Apellido:${usuario.name.last}-ID:${id.substr(id.length-6)} - ${ahora}`);
     }
+//    console.log(arrayuser[1]);
 })
+
+// modificar los log de la consola con chalk
+//console.log(chalk.blue.bgWhite('servidor funcionando en el puerto 3000'));
 
